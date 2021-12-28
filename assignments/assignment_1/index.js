c24375a46302ba7dda6121e4279d03b9044d5741
chitha_srinath <chithasaireddy@gmail.com>
2021-12-28 14:19:29 +0530

    task completed

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,13 +1,21 @@
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
+    return process.argv[process.argv.length - 1]
 }
 
 function getNameFromEnv() {
     // Write your code here
+    return process.env.name
 }
 
 function getNameFromReadLine() {
     // Write your code here
+    const readline = require("readline");
+    const rl = readline.createInterface(process.stdin,process.stdout);
+    rl.question("what is your name? ", (name) => {
+        console.log(name);
+        rl.close();
+    });
 }
 
 module.exports = {
